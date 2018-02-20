1b6cdc3a0d7345d4a42457fddd619677f4abe310
choudam <36389064+choudam@users.noreply.github.com>
2018-02-20 10:08:09 -0800

    new version

--- examples/PrimeNumbers.py
+++ examples/PrimeNumbers.py
@@ -1,29 +1,146 @@
 import math
+import time
+#import threading
 
-def Nprime(cnt:int):
+
+def Nprime(cnt:int, segsize:int):
     nextNumber = 2;
     primelist = [];
 
     while cnt > 0:
             isprime = True;
-            sqrtnum = math.sqrt(nextNumber);
-            for prm in primelist:
+            endnum = nextNumber - 1;
+            for prm in range(2, endnum):
                 if (nextNumber % prm == 0):
                     isprime = False;
                     break;
-                if (prm >= sqrtnum):
+                if (prm >= endnum):
                     break;
             if (isprime):
                 primelist.append(nextNumber);
-                print(cnt,':', nextNumber);
+                if( cnt % segsize == 0):
+                    print(cnt,':', nextNumber);
+                cnt-=1
+            nextNumber+=1;
+    return primelist;
+
+def NprimeSqrt(cnt:int, segsize:int):
+    nextNumber = 2;
+    primelist = [];
+
+    while cnt > 0:
+            isprime = True;
+            endnum = int(math.sqrt(nextNumber)) + 1;
+            for prm in range(2, endnum):
+                if (nextNumber % prm == 0):
+                    isprime = False;
+                    break;
+                if (prm >= endnum):
+                    break;
+            if (isprime):
+                primelist.append(nextNumber);
+                if( cnt % segsize == 0):
+                    print(cnt,':', nextNumber);
                 cnt-=1
             nextNumber+=1;
     return primelist;
 
 
+def NprimeSqrtPrimes(cnt:int, segsize:int):
+    nextNumber = 2;
+    primelist = [];
 
-cnt =0;
-cnt = int(input("How many prime numbers you want:"));
+    while cnt > 0:
+            isprime = True;
+            endnum = math.sqrt(nextNumber);
+            for prm in primelist:
+                if (nextNumber % prm == 0):
+                    isprime = False;
+                    break;
+                if (prm >= endnum):
+                    break;
+            if (isprime):
+                primelist.append(nextNumber);
+                if( cnt % segsize == 0):
+                    print(cnt,':', nextNumber);
+                cnt-=1
+            nextNumber+=1;
+    return primelist;
 
-print (Nprime(cnt));
+#nextNumber = 2;
+#primelist = [];
+#lock = threading.Lock()
+#primenumcount = 0
+# 
+#def NprimeSqrtPrimesMultiThreads(cnt:int):
+#    global primenumcount
+#    global nextNumber
+#    global primelist
+#    primenumcount = cnt
+#    nextNumber = 2;
+#    primelist = [];
+#    
+#    threadscount = 8
+#    threads = []
+#    for i in range(threadscount):
+#        t = threading.Thread(target=worker, args=(i,))
+#        threads.append(t)
+#        t.start()
+#
+#    for i in range(threadscount):
+#        threads[i].join()
+#    return;
+#
+#def worker(thid:int):
+#    global primenumcount
+#    global nextNumber
+#    global primelist
+#    while primenumcount > 0:
+#            isprime = True;
+#            endnum = math.sqrt(nextNumber);
+#            prmidx = 0
+#            while(isprime):
+#                if (nextNumber % primelist[prmidx] == 0):
+#                    isprime = False;
+#                    break;
+#                if (primelist[prmidx] >= endnum):
+#                    break;
+#                if (prmidx < primelist.count):
+#                    prmidx += 1
+#                
+#            
+#            lock.acquire()
+#            if (isprime):                
+#                primelist.append(nextNumber);
+#                if( primenumcount % 10000 == 0):
+#                    print(thid, '-', primenumcount,':', nextNumber);
+#                primenumcount-=1                
+#            nextNumber+=1;
+#            lock.release()
+#    return
+#    
+
+
+cnt = 10000
+segsize = 1000
+#cnt = int(input("How many prime numbers you want:"));
+
+startTime = time.time()
+Nprime(cnt,segsize);
+print('Time elapsed for Nprime: %d - %f \n' % (cnt, time.time() - startTime))
+
+startTime = time.time()
+NprimeSqrt(cnt,segsize);
+print('Time elapsed for NprimeSqrt: %d - %f \n' % (cnt, time.time() - startTime))
+
+startTime = time.time()
+NprimeSqrtPrimes(cnt,segsize);
+print('Time elapsed for NprimeSqrtPrimes: %d - %f \n' % (cnt, time.time() - startTime))
+
+
+#startTime = time.time()
+#NprimeSqrtPrimesMultiThreads(cnt);
+#print('Time elapsed for NprimeSqrtPrimesMultiThreads: %d - %f \n' % (cnt, time.time() - startTime))
+
+
 
--- examples/SumNFirstNumbers.py
+++ examples/SumNFirstNumbers.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import threading
-import ctypes
+#import ctypes
 import time
 
-w32 = ctypes.windll.kernel32
-
-THREAD_PRIORITY_NORMAL = 0
-THREAD_PRIORITY_ABOVE_NORMAL = 1
-THREAD_PRIORITY_HIGHEST = 2
-HIGH_PRIORITY_CLASS = 0x00000080
-NORMAL_PRIORITY_CLASS = 0x00000020
-
-
-result = w32.SetPriorityClass(w32.GetCurrentProcess(), HIGH_PRIORITY_CLASS)
-if not result:
-    print ('Failed to set priority of thread', w32.GetLastError())
+#w32 = ctypes.windll.kernel32
+#
+#THREAD_PRIORITY_NORMAL = 0
+#THREAD_PRIORITY_ABOVE_NORMAL = 1
+#THREAD_PRIORITY_HIGHEST = 2
+#HIGH_PRIORITY_CLASS = 0x00000080
+#NORMAL_PRIORITY_CLASS = 0x00000020
+#
+#
+#result = w32.SetPriorityClass(w32.GetCurrentProcess(), HIGH_PRIORITY_CLASS)
+#if not result:
+#    print ('Failed to set priority of thread', w32.GetLastError())
         
 startTime = time.time()
 finalsum = 0
 lock = threading.Lock()
 
 def worker(num,bi,ei):
+    starttime = time.time()
     global finalsum
-    global lock 
-    result = w32.SetThreadPriority(w32.GetCurrentThread(), THREAD_PRIORITY_HIGHEST)
-    if not result:
-        print ('Failed to set priority of thread', w32.GetLastError())
-          
+#    global lock 
+#    result = w32.SetThreadPriority(w32.GetCurrentThread(), THREAD_PRIORITY_HIGHEST)
+#    if not result:
+#        print ('Failed to set priority of thread', w32.GetLastError())
+#          
     ei+=1
     tempsum = 0
 #    i = bi
@@ -35,16 +36,18 @@ def worker(num,bi,ei):
 #        tempsum += i
 #        i += 1
     for i in range(bi,ei):
-        tempsum = tempsum + i
+        tempsum +=  i
+    
     lock.acquire()
     finalsum += tempsum
+    print('Time elapsed: %d %f\n' % (num, time.time() - starttime))
     lock.release()
     return
 
-threadscount = 4
-numcount = 1000000000
+threadscount = 8
+numcount = 1000000
 ransize = int(numcount / threadscount)
-print('ransize %d' % ransize)
+print('ransize %d\n' % ransize)
 
 threads = []
 for i in range(threadscount):
@@ -59,8 +62,7 @@ for i in range(threadscount):
 for i in range(threadscount):
     threads[i].join()
     
-print(finalsum)
-print('Time elapsed:', time.time() - startTime)
+print('Time elapsed: %d %f \n' % (finalsum, time.time() - startTime))
 
 
     
